bcb5bf3dbe827840f910c72e66a48820499bb5ba
mansi0 <mansimandhare.mm@gmail.com>
2021-11-29 16:25:53 +0530

    solution of balance partition in c

--- balance_partitioning/balance_partitioning_soln.c
+++ balance_partitioning/balance_partitioning_soln.c
@@ -8,11 +8,38 @@ int main(int argc,char* argv[]) {
         exit(1);
 
     }
-    double n = atoi(argv[1]);
+    int n = atoi(argv[1]);
 
-    double base = log2l(n);
-    //printf("\n%lf",base);
+    /*double base = log2l(n);
+    printf("\n%lf",base);
     int diff = n-floor(base);
-    printf("%d\n",diff);
+    printf("%d\n",diff);*/
+
+    int index = ceil(n*2/3) -1;
+
+    int a1=4,d=2,n1=index-1;
+    int s1 = (n1/2)*(2*a1+(n1-1)*d);
+
+    int a2 = (2*index)+4 , n2 = n-index;
+    int s2 = (n2/2)*(2*a2+(n2-1)*d);
+
+    int diff = abs(s1-s2);
+    int cal_min_sum = 0;
+    while(cal_min_sum < diff) {
+        //printf("%d ",diff);
+        diff = cal_min_sum==0 ? diff : cal_min_sum;
+        index += 1;
+        n1=index-1;
+        s1 = (n1/2)*(2*a1+(n1-1)*d);
+
+        a2 = (2*index)+4 ;
+        n2 = n-index;
+        s2 = (n2/2)*(2*a2+(n2-1)*d);
+        
+        cal_min_sum = abs(s1-s2);
+        //printf(" %d\n",cal_min_sum);
+
+    }
+    printf("%d\n",index);
 
 }
